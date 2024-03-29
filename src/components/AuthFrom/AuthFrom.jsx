983ba8e03e066756d605e920b8a3678527c3c4bc
Varun <Varun.ramesh1018@gmail.com>
2024-03-29 15:40:42 +0530

    google auth added

--- src/components/AuthFrom/AuthFrom.jsx
+++ src/components/AuthFrom/AuthFrom.jsx
@@ -35,7 +35,7 @@ const AuthFrom = () => {
             </Text>
             <Box flex={2} h={"1px"} bg={"gray.400"} />
           </Flex>
-         <GoogleAuth />
+         <GoogleAuth  prefix={isLogin?"Log in":"Sign up"} />
         </VStack>
       </Box>
       <Box border={"1px solid gray"} borderRadius={4} padding={5}>
